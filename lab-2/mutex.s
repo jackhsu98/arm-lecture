ae02d3235e297419f3f9c8d29e0fd605e727f40f
jackhsu98 <jackhsu98@gmail.com>
2014-09-29 15:57:01 +0800

    Pseudocode for mutex_lock has done !

--- lab-2/mutex.s
+++ lab-2/mutex.s
@@ -4,14 +4,25 @@
 
 	.equ locked, 1
 	.equ unlocked, 0
+	.equ successful, 0
+	.equ unsuccessful, 1
 
 	.global lock_mutex
 	.type lock_mutex, function
 lock_mutex:
         @ INSERT CODE BELOW
 
+        ldr r2, =locked    @ To load locked value
+.L1:
+		ldrex r1, [r0]     @ LDREX from [r0]
+		cmp r1, #locked    @ compare to unlocked value
+		beq .L1
+		strex r1, r2, [r0]
+		cmp r1, #successful
+		bne .L1
+
         @ END CODE INSERT
-	bx lr
+		bx lr
 
 	.size lock_mutex, .-lock_mutex
 
@@ -19,8 +30,10 @@ lock_mutex:
 	.type unlock_mutex, function
 unlock_mutex:
 	@ INSERT CODE BELOW
+	ldr r1, =unlocked
+	str r1, [r0]
         
-        @ END CODE INSERT
+    @ END CODE INSERT
 	bx lr
 	.size unlock_mutex, .-unlock_mutex
 
